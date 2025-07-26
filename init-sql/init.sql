9bf067f491bdfb4a8c8d6411362cd2f173acf1b7
jbg <jbg4354@naver.com>
2025-07-26 13:07:05 +0900

    update

--- init-sql/init.sql
+++ init-sql/init.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE user_service;
 CREATE USER user_service_user WITH PASSWORD 'test123!';
 GRANT ALL PRIVILEGES ON DATABASE user_service TO user_service_user;
-GRANT USAGE, CREATE ON SCHEMA public TO user_service_user;
+ALTER SCHEMA public OWNER TO user_service_user;
 
 CREATE DATABASE cart_service;
 CREATE USER cart_service_user WITH PASSWORD 'test123!';
 GRANT ALL PRIVILEGES ON DATABASE cart_service TO cart_service_user;
-GRANT USAGE, CREATE ON SCHEMA public TO cart_service_user;
+ALTER SCHEMA public OWNER TO cart_service_user;
 
 CREATE DATABASE product_service;
 CREATE USER product_service_user WITH PASSWORD 'test123!';
 GRANT ALL PRIVILEGES ON DATABASE product_service TO product_service_user;
-GRANT USAGE, CREATE ON SCHEMA public TO product_service_user;
+ALTER SCHEMA public OWNER TO product_service_user;
 
 CREATE DATABASE order_service;
 CREATE USER order_service_user WITH PASSWORD 'test123!';
 GRANT ALL PRIVILEGES ON DATABASE order_service TO order_service_user;
-GRANT USAGE, CREATE ON SCHEMA public TO order_service_user;
+ALTER SCHEMA public OWNER TO order_service_user;
 
 CREATE DATABASE pay_service;
 CREATE USER pay_service_user WITH PASSWORD 'test123!';
 GRANT ALL PRIVILEGES ON DATABASE pay_service TO pay_service_user;
-GRANT USAGE, CREATE ON SCHEMA public TO pay_service_user;
+ALTER SCHEMA public OWNER TO pay_service_user;
 
 
